af9f88a8e626d0491484569d9c29d1c1ab613f28
ytserilo <33002689+ytserilo@users.noreply.github.com>
2020-01-13 13:52:29 +0200

    Update routing.py

--- routing.py
+++ routing.py
@@ -2,6 +2,6 @@ from telegram.messanger import *
 from base.base import get_card_token, app
 
 app.add_url_rule(
-    '/6943ad10abed4fcc84998e6d7ebcf2ae201bb502b65f4109ac03f73a4c5f54d2c0d2d0ead29941769162848224eae000',
+    '/<secret key>',
     view_func=TelegramMessanger.as_view('telegram_messanger')
 )
